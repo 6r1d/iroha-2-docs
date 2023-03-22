ec66d24dc4c888dea51367aa4bb2ce1179394749
6r1d <vic.6r1d@gmail.com>
2023-03-22 08:59:33 +0200

    [chore] Update analytics with vue-gtm
    Signed-off-by: 6r1d <vic.6r1d@gmail.com>

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 
 import ThemeDefault from 'vitepress/theme'
 import LayoutCustom from './components/LayoutCustom.vue'
+import { createGtm } from '@gtm-support/vue-gtm'
 
 import 'virtual:windi.css'
 import './style/index.scss'
@@ -9,4 +10,19 @@ import './style/index.scss'
 export default {
   ...ThemeDefault,
   Layout: LayoutCustom,
+  enhanceApp({ app, router }) {
+    app.use(
+      createGtm({
+        id: 'GTM-7XJ4SJS4KG',
+        defer: false,
+        compatibility: false,
+        enabled: true,
+        debug: true,
+        loadScript: true,
+        vueRouter: router,
+        ignoredViews: ['homepage'],
+        trackOnNextTick: false,
+      }),
+    )
+  },
 }
